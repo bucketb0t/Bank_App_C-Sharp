9ece201e2326cf6793cab805bb4f6718bdff673b
bucketb0t <dima.sebastian.leonard@gmail.com>
2024-05-12 15:25:50 +0300

    main: Login window initialization

---
+++ Bank_App/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //display title
+        System.Console.WriteLine("***bucketb0t Baank***");
+        System.Console.WriteLine("::Login Page::");
+
+        //variables to store username and password
+        string userName = null, password = null;
+
+        //read userName from keyboard
+        System.Console.Write("Username: ");
+        userName = System.Console.ReadLine();
+
+
+        //read password from keyboard only if username is entered
+        if (userName != "")
+        {
+            System.Console.Write("Password:");
+            userName = System.Console.ReadLine();
+        }
+
+        //check username and password
+        if (userName == "system" && password == "manager")
+        {
+            System.Console.WriteLine("TO Do: Main Menu Here");
+        }
+
+        //about to exit
+        System.Console.WriteLine("Thank you! Visit again.");
+        System.Console.ReadKey();
+    }
+
+}
