27ac24aaa04c3fafac4134da4d2594e6378bb80b
bucketb0t <dima.sebastian.leonard@gmail.com>
2024-05-12 15:50:23 +0300

    main: Main Menu initialization

--- Bank_App/Program.cs
+++ Bank_App/Program.cs
@@ -18,13 +18,50 @@
         if (userName != "")
         {
             System.Console.Write("Password:");
-            userName = System.Console.ReadLine();
+            password = System.Console.ReadLine();
         }
-
+        
         //check username and password
-        if (userName == "system" && password == "manager")
+        if (userName=="system" && password== "manager")
         {
-            System.Console.WriteLine("TO Do: Main Menu Here");
+            //declare variable to store menu choice
+            int mainMenuChoice = -1;
+
+            do
+            {
+                //show main menu
+                System.Console.WriteLine("\n ::Main Menu::");
+                System.Console.WriteLine("1. Customers");
+                System.Console.WriteLine("2. Accounts");
+                System.Console.WriteLine("3. Funds Transfer");
+                System.Console.WriteLine("4. Funds Transfer Statement");
+                System.Console.WriteLine("5. Account Statement");
+                System.Console.WriteLine("0. Exit");
+
+
+                //accept menu choice from keyboard
+                System.Console.Write("Enter choice:");
+                mainMenuChoice = int.Parse(System.Console.ReadLine());
+
+                //switch-case to check menu choice1
+                switch (mainMenuChoice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                }
+            } while (mainMenuChoice != 0);
+        }
+        else
+        {
+            System.Console.WriteLine("Invalid username or password!");
         }
 
         //about to exit
