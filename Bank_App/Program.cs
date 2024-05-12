bf48d4055241e9abc0f57d90d124b7940c16bbaa
bucketb0t <dima.sebastian.leonard@gmail.com>
2024-05-12 16:20:05 +0300

    main: Custumers Menu, and Accounts Menu initialization

--- Bank_App/Program.cs
+++ Bank_App/Program.cs
@@ -46,10 +46,8 @@
                 //switch-case to check menu choice1
                 switch (mainMenuChoice)
                 {
-                    case 1:
-                        break;
-                    case 2:
-                        break;
+                    case 1: CustomersMenu();break;
+                    case 2: AccountsMenu(); break;
                     case 3:
                         break;
                     case 4:
@@ -69,4 +67,44 @@
         System.Console.ReadKey();
     }
 
+    static void CustomersMenu()
+    {
+        int customersMenuChoice = -1;
+
+        //do-while loop starts
+
+        do
+        {
+            System.Console.WriteLine("\n::Customers Menu::");
+            System.Console.WriteLine("1. Add Customer");
+            System.Console.WriteLine("2. Delete Customer");
+            System.Console.WriteLine("3. Update Customer");
+            System.Console.WriteLine("4. View Customers");
+            System.Console.WriteLine("0. Back to Main Menu");
+
+            //accept customers menu choice
+            System.Console.Write("Enter choice:");
+            customersMenuChoice = System.Convert.ToInt32(System.Console.ReadLine());
+        } while (customersMenuChoice != 0);
+    }
+
+    static void AccountsMenu() {
+        int accountsMenuChoice = -1;
+
+        //do-while loop starts
+
+        do
+        {
+            System.Console.WriteLine("\n::Customers Menu::");
+            System.Console.WriteLine("1. Add Account");
+            System.Console.WriteLine("2. Delete Account");
+            System.Console.WriteLine("3. Update Account");
+            System.Console.WriteLine("4. View Accounts");
+            System.Console.WriteLine("0. Back to Main Menu");
+
+            //accept customers menu choice
+            System.Console.Write("Enter choice:");
+            accountsMenuChoice = System.Convert.ToInt32(System.Console.ReadLine());
+        } while (accountsMenuChoice != 0);
+    }
 }
